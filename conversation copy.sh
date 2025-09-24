4df5a931efe0d51c06a45a09b71093b5da3914bd
sowjanya <sowji.muppala@gmail.com>
2025-09-24 22:23:51 +0530

    addd

---
+++ conversation copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=raju
+Preson2=sachin
+
+echo "hello $Person1"
+echo "hello $Preson2"
+echo "$Preson2 which course r you doing now"
+echo "$Person1: Im learning hell script"
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "hello $Person1"
+echo "hello $Preson2"
+echo "$Preson2 which course r you doing now"
+echo "$Person1: Im learning hell script"
