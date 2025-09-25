82d744b21342a480a1e42ffbba1a3ac459c09aa4
sowjanya <sowji.muppala@gmail.com>
2025-09-25 10:16:39 +0530

    addd

--- secialvar.sh
+++ secialvar.sh
@@ -3,6 +3,9 @@
  echo "all variables passed to the script is :$*"
 
  echo "script name is :$0"
- echo "current directory name is :$pwd"
+ echo "current directory name is :$PWD"
  echo "who is running the script name is :$USER"
  echo "home directory of the user:$HOME"
+ echo "processID of the script :$$"
+ sleep 10 &
+ echo "process id of the back ground script :$!"
