b26040d5a2ae7bbc964d6783604595de088c6810
sowjanya <sowji.muppala@gmail.com>
2025-09-25 18:10:02 +0530

    addd

--- installationex.sh
+++ installationex.sh
@@ -3,12 +3,14 @@
 userid=$(id -u)
 if [ $userid -ne 0 ]; then
     echo "ERROR:: please proceed with root user privilage"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo"ERROR:: installation failed"
+    exit 1
 else
     echo"installing MySql is Success"
 fi
