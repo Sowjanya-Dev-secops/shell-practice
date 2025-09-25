465b303b2e9e0c325065e418adfe36ac7461114b
sowjanya <sowji.muppala@gmail.com>
2025-09-25 10:13:23 +0530

    addd

--- secialvar.sh
+++ secialvar.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
  echo "all variables passed to the script is :$@"
  echo "all variables passed to the script is :$*"
+
+ echo "script name is :$0"
+ echo "current directory name is :$pwd"
+ echo "who is running the script name is :$USER"
+ echo "home directory of the user:$HOME"
