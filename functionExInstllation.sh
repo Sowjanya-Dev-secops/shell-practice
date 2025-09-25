64b5f1cda4d20e1b99ff6b2b2776ff14b6e8259d
sowjanya <sowji.muppala@gmail.com>
2025-09-25 20:53:07 +0530

    addd

--- functionExInstllation.sh
+++ functionExInstllation.sh
@@ -6,12 +6,14 @@ if [ $userid -ne 0 ]; then
     echo "ERROR:: proced with root user previlage"
     exit 1
 fi
+
 validate(){
     if [ $1 -ne 0 ]; then 
         echo "Error:: $2 installation is failled"
         exit 1
     else 
         echo "$2 installation is suuccessful"
+    fi
 }
 dnf install mysql -y
 validate $? "mysql"
