1f0f592dcc23adf776a75a2beb3c6a58fe2c6e07
sowjanya <sowji.muppala@gmail.com>
2025-09-25 20:55:41 +0530

    addd

--- functionExInstllation.sh
+++ functionExInstllation.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+G=\e[31m
+N=\e[37m
 
 userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
-    echo "ERROR:: proced with root user previlage"
+    echo  -e "$G ERROR:: $N proced with root user previlage"
     exit 1
 fi
 
