8cb783049795a2c3a755ccebf85ffc8b1fc9bb0d
sowjanya <sowji.muppala@gmail.com>
2025-09-26 15:18:11 +0530

    add..

---
+++ forloopInstalation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+user=$(id -u)
+
+if [ $? -ne 0 ]; then
+    echo "$R Error:: $N Please proceed with root user "
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error:: $N $2 Installation failure"
+        exit 1
+    else
+        echo -e "$G Success:: $N $2 installation is successful" 
+}
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then
+    dnf instlled mysql -y
+    validate $? "mysql"
+else
+    echo -n "mysql is already installed .. $Y SKipp$N"
