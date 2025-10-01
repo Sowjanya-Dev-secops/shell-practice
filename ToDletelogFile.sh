09b53ed57742c31b38189fdfb28f59b79239ee0c
sowjanya <sowji.muppala@gmail.com>
2025-10-01 16:52:04 +0530

    add

---
+++ ToDletelogFile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+Source_dir="/home/ec2-user/app-logs"
+
+if [ ! -d $SCRIPT_DIR ]; then
+    echo "ERROR:: SCRIPT_DIR does not exist"
+    exit 1
+fi
+
+file_to_del=$(find $Source_dir -name "*.log" )
+while IFS= read -r file_path
+do
+    echo "deleting the $file_path"
+    rm -rf $file_path
+    echo "deleted the $file_path"
+done <<< $Source_dir
